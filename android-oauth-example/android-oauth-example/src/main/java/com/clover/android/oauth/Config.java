3e3a1eada2275764648dc44feeb0ccf65f2cbd76
Zach Jiroun <zjiroun@gmail.com>
2014-06-23 16:01:21 -0700

    Updated Android OAuth Example

--- android-oauth-example/android-oauth-example/src/main/java/com/clover/android/oauth/Config.java
+++
@@ -1,9 +0,0 @@
-package com.clover.android.oauth;
-
-/**
- * Created by jwils on 9/6/13.
- */
-public class Config {
-    public static final String CLIENT_ID = "YOUR CLIENT ID";
-    public static final String APP_DOMAIN = "YOUR APP DOMAIN";
-}
