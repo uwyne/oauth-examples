c6c2ddf030bdb09443d2b0cb302d08d4e9d41bfb
ZachJiroun <zjiroun@umd.edu>
2014-08-22 18:36:29 -0700

    Updated OAuth Connection

--- android-oauth-example/oauthexample/app/src/main/java/com/clover/example/oauthexample/app/WebViewActivity.java
+++ android-oauth-example/oauthexample/app/src/main/java/com/clover/example/oauthexample/app/WebViewActivity.java
@@ -9,21 +9,20 @@ import android.webkit.WebViewClient;
 
 
 /**
- * Created by zach on 6/19/14.
+ * Created by Zach J. on 8/22/14.
  */
 public class WebViewActivity extends Activity {
-
-    private WebView webView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_webview);
 
+        WebView webView;
+
         // The URL that will fetch the Access Token, Merchant ID, and Employee ID
         String url = "https://clover.com/oauth/authorize" +
                 "?client_id=" + Config.APP_ID +
-                "&response_type=token" +
                 "&redirect_uri=" + Config.APP_DOMAIN;
 
         // Creates the WebView
@@ -32,21 +31,21 @@ public class WebViewActivity extends Activity {
         webView.setWebViewClient(new WebViewClient() {
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 // Parses the fetched URL
-                String accessTokenFragment = "#access_token=";
+                String authCodeFragment = "?code=";
                 String merchantIdFragment = "&merchant_id=";
                 String employeeIdFragment = "&employee_id=";
 
-                int accessTokenStart = url.indexOf(accessTokenFragment);
+                int authCodeStart = url.indexOf(authCodeFragment);
                 int merchantIdStart = url.indexOf(merchantIdFragment);
                 int employeeIdStart = url.indexOf(employeeIdFragment);
-                if (accessTokenStart > -1) {
-                    String accessToken = url.substring(accessTokenStart + accessTokenFragment.length(), merchantIdStart);
+                if (authCodeStart > -1) {
+                    String accessToken = url.substring(authCodeStart + authCodeFragment.length(), merchantIdStart);
                     String merchantId = url.substring(merchantIdStart + merchantIdFragment.length(), employeeIdStart);
                     String employeeId = url.substring(employeeIdStart + employeeIdFragment.length(), url.length());
 
                     // Sends the info back to the MainActivity
                     Intent output = new Intent();
-                    output.putExtra(MainActivity.ACCESS_TOKEN_KEY, accessToken);
+                    output.putExtra(MainActivity.AUTH_CODE, accessToken);
                     output.putExtra(MainActivity.MERCHANT_ID_KEY, merchantId);
                     output.putExtra(MainActivity.EMPLOYEE_ID_KEY, employeeId);
                     setResult(RESULT_OK, output);
