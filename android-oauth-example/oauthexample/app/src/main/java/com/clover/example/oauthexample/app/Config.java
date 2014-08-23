27a619beab3dc3646fed703980da978a88042c31
ZachJiroun <zjiroun@umd.edu>
2014-08-22 18:32:40 -0700

    Update Config.java

--- android-oauth-example/oauthexample/app/src/main/java/com/clover/example/oauthexample/app/Config.java
+++ android-oauth-example/oauthexample/app/src/main/java/com/clover/example/oauthexample/app/Config.java
@@ -7,5 +7,5 @@ public class Config {
     // Your APP_ID can be found on your developers account
     public static final String APP_ID = "Your App ID";
     // APP_DOMAIN is some dummy domain
-    public static final String APP_DOMAIN = "http://localhost";
+    public static final String APP_DOMAIN = "http://example.com";
 }
